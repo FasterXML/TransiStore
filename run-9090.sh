d909dfe926b5de929d057db4d2986914ac4f61be
Tatu Saloranta <tsaloranta@gmail.com>
2013-01-10 14:49:21 -0800

    adding startup script

---
+++ run-9090.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+java -Xmx1024M -Xms1024M -XX:-UseParallelOldGC \
+  -Ddw.http.port=9090 \
+  -Ddw.http.adminPort=9090 \
+  -jar ts-server/target/transistore-server-0.5.0-SNAPSHOT.jar \
+  server ./sample/single-node-8080.yml
