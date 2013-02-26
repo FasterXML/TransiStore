d7a0b1f687ef7512196ab17d745580111b6430ea
Tatu Saloranta <tsaloranta@gmail.com>
2013-02-25 19:54:28 -0800

    ...

--- run-9090.sh
+++ run-9090.sh
@@ -2,5 +2,5 @@
 java -Xmx1024M -Xms1024M -XX:-UseParallelOldGC \
   -Ddw.http.port=9090 \
   -Ddw.http.adminPort=9090 \
-  -jar ts-server/target/transistore-server-0.6.0-SNAPSHOT.jar \
+  -jar ts-server/target/transistore-server-0.7.0-SNAPSHOT.jar \
   server ./sample/single-node-8080.yml
