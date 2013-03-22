7df1dd82a50359a7f5c782b968a1e744658c3d67
Tatu Saloranta <tsaloranta@gmail.com>
2013-03-22 16:51:51 -0700

    minor cleanup

--- tstore.sh
+++ tstore.sh
@@ -4,6 +4,6 @@
 # all configs there may be assuming 9090 is one of hosts!
 # Also note that jar SHOULD be real executable (on unix anyway)
 
-CONFIG="sample/single-node-8080.yml"
+CONFIG="sample/single-node-9090.yml"
 ./ts-commands/target/transistore-commands-0.9.3-SNAPSHOT.jar \
  --config-file $CONFIG $@
