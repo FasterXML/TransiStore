22af3d0aab35394c371d565353792c5bb4f3d82c
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-02-08 19:58:56 -0800

    Add bit more client config

--- tstore.sh
+++ tstore.sh
@@ -4,6 +4,12 @@
 # all configs there may be assuming 9090 is one of hosts!
 # Also note that jar SHOULD be real executable (on unix anyway)
 
-CONFIG="sample/single-node-9090.yml"
+# let's actually use 4-node config, even for other clusters; more likely to succeed
+# bootstrapping
+
+#CONFIG=sample/single-node-9090.yml
+CONFIG=sample/four-nodes-4copies.yml
+
 ./ts-commands/target/transistore-commands-0.9.9-SNAPSHOT.jar \
  --config-file $CONFIG $@
+
