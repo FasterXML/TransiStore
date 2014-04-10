49111f00ea4072b5c0aacfb0665880f7066e27eb
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-04-10 15:00:43 -0700

    update run scripts

--- profile-9090.sh
+++ profile-9090.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 java -Xmx1024M -Xms1024M -Xmn512M -XX:-UseParallelOldGC \
   -Xrunhprof:cpu=samples,depth=15,verbose=n,interval=2 \
-  -Ddw.http.port=9090 \
-  -Ddw.http.adminPort=9090 \
+  -Ddw.server.connector.port=9090 \
   -jar ts-server/target/transistore-server-0.11.0-SNAPSHOT.jar \
   server ./sample/single-node-9090.yml
--- run-leveldb-9090.sh
+++ run-leveldb-9090.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 java -Xmx1024M -Xms1024M -Xmn512M -XX:-UseParallelOldGC \
-  -Ddw.http.port=9090 \
-  -Ddw.http.adminPort=9090 \
+  -Ddw.server.connector.port=9090 \
   -jar ts-server/target/transistore-server-0.11.0-SNAPSHOT.jar \
   server ./sample/single-leveldb-9090.yml
