0b2ad90ea6f17349f982b335875d1d5ad2278900
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-09-04 18:32:39 -0700

    add runner scripts for dr case

---
+++ 2way-dr/run-secondary.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PORT=$1
+if [ "$PORT" == "" ]; then
+  echo "Missing port argument, can not run"
+  exit 1
+fi
+
+java -Xmx1024M -Xms1024M -XX:-UseParallelOldGC \
+  -Ddw.server.connector.port=$PORT \
+  -jar ../../ts-server/target/transistore-server-0.11.0-SNAPSHOT.jar \
+  server ../../sample/two-nodes-dr2.yml
